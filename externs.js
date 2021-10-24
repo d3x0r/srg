7997a6e3fdfb8b7ca0e07c236a5081f3c6e97fc0
d3x0r <d3x0r@no-reply.github.com>
2021-10-23 17:18:36 -0700

    Revert changes to K12Module; was trying to reduce preload overhead, since it's mostly just an alogrithm

--- externs.js
+++ externs.js
@@ -1,1 +1,2 @@
 const Buffer = {};
+const FS={};
--- test.js
+++ test.js
@@ -4,3 +4,6 @@ import {SaltyRNG} from "./salty_random_generator.js"
 
 console.log( "Salty:", SaltyRNG );
 console.log( "ALL:", SRG );
+
+console.log( "Pass:", SaltyRNG.id("pass") );
+console.log( "Password:", SaltyRNG.id("password") );
