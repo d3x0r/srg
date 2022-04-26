67f2ffe0b85820e9e126b849cd8fbe0a73e2a178
d3x0r <d3x0r@no-reply.github.com>
2022-04-26 02:05:01 -0700

    Update test profile to include short rng's

--- test/test1.js
+++ test/test1.js
@@ -1,4 +1,13 @@
 
+import {SaltyRNG} from "../salty_random_generator.mjs"
+import {SFC32,MUL32,XOR32,JSF32} from "../prng_short.mjs"
+
+const sfc = SFC32( "seed" );
+const mul = MUL32( "seed" );
+const xor = XOR32( "seed" );
+const jsf = JSF32( "seed" );
+//const sfc = MUL32( "seed" );
+
 const size = 256;
 const bits = 8;
 
@@ -13,42 +22,68 @@ if( !frameCounter ) {
 }
 frameCounter.textContent = "?? FPS";
 
+const enables = { enable:true, enable2:true, enable3:true, enable4:true }
+
 var canvas = document.getElementById( "graphout" );
 var ctx= canvas.getContext( "2d" );
 canvas.width = size;
 canvas.height = size;
 var myImageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
 var myEnable = document.getElementById( "enablegraphout" );
-var enables = { enable:true, enable2:true, enable3:true }
 myEnable.addEventListener( "click", ()=>{
 	enables.enable = !enables.enable;
 	firstTick = Date.now();
 	frames = 0;
 } );
 
-var canvas2 = document.getElementById( "graphout2" );
-var ctx2= canvas2.getContext( "2d" );
-canvas2.width = size;
-canvas2.height = size;
-var myImageData2 = ctx2.getImageData(0, 0, canvas2.width, canvas2.height)
-var myEnable2 = document.getElementById( "enablegraphout2" );
-myEnable2.addEventListener( "click", ()=>{
-	enables.enable2 = !enables.enable2;
-	firstTick = Date.now();
-	frames = 0;
-} );
+function initCanvas( n ) {
+let created = false;
+const canvas = document.getElementById( "graphout"+n )||((created=true),(document.createElement("canvas")));
+if( created ) {
+	const enable_cell = document.createElement( "td" );
+	//cell.appendChild( canvas );
+//	const 
 
-var canvas3 = document.getElementById( "graphout3" );
-var ctx3= canvas3.getContext( "2d" );
-canvas3.width = size;
-canvas3.height = size;
-var myImageData3 = ctx3.getImageData(0, 0, canvas3.width, canvas3.height)
-var myEnable3 = document.getElementById( "enablegraphout3" );
-myEnable3.addEventListener( "click", ()=>{
-	enables.enable3 = !enables.enable3;
-	firstTick = Date.now();
-	frames = 0;
-} );
+	const cell = document.createElement( "td" );
+	cell.appendChild( canvas );
+	document.body.appendChild( cell );
+	document.body.appendChild( canvas );
+}
+var ctx= canvas.getContext( "2d" );
+canvas.width = size;
+canvas.height = size;
+var myImageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+if( !created ) {
+	var myEnable2 = document.getElementById( "enablegraphout"+n );
+	myEnable2.addEventListener( "click", ()=>{
+		enables["enable"+n] = !enables["enable"+n];
+		firstTick = Date.now();
+		frames = 0;
+	} );
+}
+
+	
+const values = [];
+const values_acc = [];
+for( let n = 0; n < size; n++ )  {
+	var newCol;
+	var newCol_acc;
+	values.push( newCol=[] );
+	values_acc.push( newCol_acc=[] );
+	for( var m = 0; m < size; m++ )  {
+		newCol.push( 0 );
+		newCol_acc.push( 0 );
+    }        
+}
+
+	return { canvas, ctx, myImageData, values, values_acc }
+
+}
+
+const test2 = initCanvas( 2 );
+const test3 = initCanvas( 3 );
+const test4 = initCanvas( 4 );
+
 
 var values = [];
 var values_acc = [];
@@ -63,39 +98,13 @@ for( var n = 0; n < size; n++ )  {
     }        
 }
 
-var values2 = [];
-var values2_acc = [];
-for( var n = 0; n < size; n++ )  {
-	var newCol;
-	var newCol_acc;
-	values2.push( newCol=[] );
-	values2_acc.push( newCol_acc=[] );
-	for( var m = 0; m < size; m++ )  {
-		newCol.push( 0 );
-		newCol_acc.push( 0 );
-    }        
-}
 
-var values3 = [];
-var values3_acc = [];
-for( var n = 0; n < size; n++ )  {
-	var newCol;
-	var newCol_acc;
-	values3.push( newCol=[] );
-	values3_acc.push( newCol_acc=[] );
-	for( var m = 0; m < size; m++ )  {
-		newCol.push( 0 );
-		newCol_acc.push( 0 );
-    }        
-}
-
-
-var RNG = exports.SaltyRNG( 
+var RNG = SaltyRNG( 
 			//null 
 			(salt)=>{salt.push( Date.now().toString() )}
 		, { mode:0 } );
 
-var RNG2 = exports.SaltyRNG( 
+var RNG2 = SaltyRNG( 
 			//null 
 			(salt)=>{salt.push( Date.now().toString() )}
 		, { mode:1 } );
@@ -107,17 +116,25 @@ tick = Date.now();
 
 
 var rp1 = runPass.bind( {}, myImageData,ctx,values,values_acc, false, RNG, "enable")
-var rp2 = runPass.bind( {}, myImageData2,ctx2,values2,values2_acc,false, RNG2, "enable2")
-var rp3 = runPass.bind( {}, myImageData3,ctx3,values3,values3_acc, true, null, "enable3")
+var rp2 = runPass.bind( {}, test2.myImageData,test2.ctx,test2.values,test2.values_acc,false, RNG2, "enable2")
+var rp3 = runPass.bind( {}, test3.myImageData,test3.ctx,test3.values,test3.values_acc, true, null, "enable3")
+var rp4 = runPass.bind( {}, test4.myImageData,test4.ctx,test4.values,test4.values_acc, true, null, "enable4", true)
+//var rp5 = runPass.bind( {}, test5.myImageData,ctx4,values4,values4_acc, true, null, "enable5", true)
 
 	var max = 0;
 	
-function runPass( myImageData,ctx,  values, values_acc, math, RNG, enable ) {
+function runPass( myImageData,ctx,  values, values_acc, math, RNG, enable, shortRNG ) {
 	if( enables[enable] ) {
 	var min = 10000;
 	tick = Date.now();
 	//while( ( newtick = Date.now() ) - tick < 300 ) {
-	if( math ) {
+	if( shortRNG ) {
+		for( var n = 0; n < size*size; n++ ) {
+			var x = (xor() * size)|0;
+			var y = (xor() * size)|0;
+			values[x][y] += 1.0;
+		}
+	} else if( math ) {
 		for( var n = 0; n < size*size; n++ ) {
 			var x = (Math.random() * size)|0;
 			var y = (Math.random() * size)|0;
@@ -249,6 +266,7 @@ function animtick() {
  rp1();
  rp2()
  rp3()
+ rp4()
 	frames++;
 	if( ( frames % 100 ) === 0 )
 		frameCounter.textContent = ( frames/ ( (Date.now()-firstTick)/1000)) + " FPS";
